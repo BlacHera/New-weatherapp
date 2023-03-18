19c4c4efe7b434f12c7763ac22a81bd59ecad137
BlacHera <121027660+BlacHera@users.noreply.github.com>
2023-03-18 09:55:42 +0100

    forecast days,icon and temperatures added

--- index.js
+++ index.js
@@ -22,30 +22,45 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily)
+let forecast = response.data.daily;
+
 let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-
 let forecastHTML=`<div class="row">`;
- days.forEach(function (day) {
+forecast.forEach(function ( forecastDay, index) {
+  if (index < 6) {
 forecastHTML= 
 forecastHTML +
     `
       <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-                  src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png"
-                  alt="Clear"
-                  width="50"
-                />
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="50"
+        />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}°</span>
         </div>
       </div>
   `;
+   }
   });
  
 forecastHTML = forecastHTML + `</div>`;
@@ -86,7 +101,7 @@ iconElement.setAttribute(
 }
 
 function searchCity(city) {
-  let apiKey = "c8a77112b2faf6684bb4b21a0aa778ae";
+  let apiKey = "4104431b5067788c689c23fb1ae31cec";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
